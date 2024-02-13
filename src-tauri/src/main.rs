94032a7756eea79abd917dc31c9eabd5841daeac
IAFEnvoy <tanksheng545@126.com>
2024-02-13 13:02:41 +0800

    add workflow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use fs_tail::TailedFile;
-use notify::{Watcher, RecursiveMode, Result,Event,EventKind};
 use notify::event::CreateKind::Any;
+use notify::{Event, EventKind, RecursiveMode, Result, Watcher};
 use std::fs::File;
 use std::io::BufRead;
 use std::path::Path;
-use std::thread::{spawn};
+use std::thread::spawn;
 use tauri::Manager;
 
 fn main() {
@@ -26,57 +26,58 @@ fn main() {
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-  data: String,
+    data: String,
 }
 
-static mut file:Option<TailedFile>=None;
-static mut file_path:String=String::new();
-static mut app_object:Option<tauri::AppHandle>=None;
+static mut FILE: Option<TailedFile> = None;
+static mut FILE_PATH: String = String::new();
+static mut APP_OBJECT: Option<tauri::AppHandle> = None;
 
 #[tauri::command]
 //Dont delete this async, or software will no response
-async fn tail_file(path:String, app: tauri::AppHandle){
+async fn tail_file(path: String, app: tauri::AppHandle) {
     //watcher is for rename event
-    let mut watcher = notify::recommended_watcher(|res:Result<Event>| {
-        match res {
-            Ok(event) => {
-                if event.kind==EventKind::Create(Any){
-                    println!("Game open triggered (File Creation), trying to re-tail");
-                    unsafe{
-                        file = Some(TailedFile::new(File::open(&file_path).unwrap()));
-                    }
-                    spawn(do_tail);
+    let mut watcher = notify::recommended_watcher(|res: Result<Event>| match res {
+        Ok(event) => {
+            if event.kind == EventKind::Create(Any) {
+                println!("Game open triggered (File Creation), trying to re-tail");
+                unsafe {
+                    FILE = Some(TailedFile::new(File::open(&FILE_PATH).unwrap()));
                 }
-            },
-            Err(e) => println!("Watch error: {:?}", e),
+                spawn(do_tail);
+            }
         }
-    }).expect("Watch file failed");
-    let _=watcher.watch(Path::new(&path), RecursiveMode::Recursive);
+        Err(e) => println!("Watch error: {:?}", e),
+    })
+    .expect("Watch file failed");
+    let _ = watcher.watch(Path::new(&path), RecursiveMode::Recursive);
     //idk how to use Box::leak lol
-    unsafe{
-        file = Some(TailedFile::new(File::open(&path).unwrap()));
-        file_path = String::from(path);
-        app_object=Some(app);
+    unsafe {
+        FILE = Some(TailedFile::new(File::open(&path).unwrap()));
+        FILE_PATH = String::from(path);
+        APP_OBJECT = Some(app);
     }
     spawn(do_tail).join().unwrap();
 }
 
-fn do_tail(){
+fn do_tail() {
     println!("Start tailing");
-    unsafe{
-        match &file {
+    unsafe {
+        match &FILE {
             Some(obj) => {
                 for line in obj.lock().lines() {
                     if let Ok(line) = line {
                         println!("{}", line);
-                        match &app_object{
-                            Some(app)=>app.emit_all("log-line", Payload { data: line.into() }).unwrap(),
-                            None => println!("None")
+                        match &APP_OBJECT {
+                            Some(app) => app
+                                .emit_all("log-line", Payload { data: line.into() })
+                                .unwrap(),
+                            None => println!("None"),
                         }
                     }
                 }
-            }, // Some: x=hello
-            None => println!("None")
+            } // Some: x=hello
+            None => println!("None"),
         }
     }
     println!("End tailing");
